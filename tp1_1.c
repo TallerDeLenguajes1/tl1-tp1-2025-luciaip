aa5809f780e4786a5e8649891cea49be429776f3
Lucia Isabel Ponce <luciaponce280606@gmail.com>
2026-04-07 11:51:51 -0300

    archivo

--- tp1_1.c
+++ tp1_1.c
@@ -3,4 +3,15 @@
 
 int main (){
     printf ("hola mundo");
+    int numero=35;
+    int tamanio;
+    int *puntero;
+    puntero = &numero;
+    printf ("\n %d",*puntero);
+    printf("\n %p", puntero);
+    printf ("\n %p", &numero);
+    printf ("\n %p", &puntero);
+    tamanio= sizeof(numero);
+    printf( "\n %d", tamanio);
+    return 0;
 }
